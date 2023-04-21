85dd7f3e882ca80abb1a335cab41079f53e0da51
wintunkyaw <wintkyaw093@gmail.com>
2023-04-21 23:04:19 +0800

    requires cleanup

--- VideoClassification/architecture/EnsembleFour.py
+++ VideoClassification/architecture/EnsembleFour.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 import torchvision
-from torchinfo import summary
 
 class DeepSearchConv(torch.nn.Module):
     def __init__(self, inputs, outputs, kernel_size):
@@ -38,21 +37,10 @@ class DeepSearchLinear(torch.nn.Module):
 class DeepSearch(torch.nn.Module):
     def __init__(self, hidden, kernel_size, window, hidden_linear, labels=2):
         super(DeepSearch, self).__init__()
-        
-        self.conv = [DeepSearchConv(hidden[i], hidden[i+1],  
-                                    kernel_size) for i in range(len(hidden)-1)]
-        self.conv_combined = nn.Sequential(*self.conv)
-        
-        self.avgpool = nn.AvgPool2d(window)
-        self.flattened = hidden_linear[0]
-        self.linear= [DeepSearchLinear(hidden_linear[i], hidden_linear[i+1]) for i in range(len(hidden_linear)-1)]
-        self.linear_combined = nn.Sequential(*self.linear)
         resnet = torchvision.models.resnet50(weights = torchvision.models.ResNet50_Weights.DEFAULT)
         alexnet = torchvision.models.alexnet(weights = torchvision.models.AlexNet_Weights.DEFAULT)
         convexnet = torchvision.models.convnext_base(weights = torchvision.models.ConvNeXt_Base_Weights.DEFAULT)
         googlenet = torchvision.models.googlenet(weights =torchvision.models.GoogLeNet_Weights.DEFAULT)
-
-        # Telling the model to keep pre-trained weights
         for param in resnet.parameters():    
             param.requires_grad = False
         for param in alexnet.parameters():
@@ -65,15 +53,31 @@ class DeepSearch(torch.nn.Module):
         self.model_2 = alexnet
         self.model_3 = convexnet
         self.model_4 = googlenet
+
+        self.batch_norm = nn.BatchNorm2d(hidden[0])
+        
+        self.conv = [DeepSearchConv(hidden[i], hidden[i+1],  
+                                    kernel_size) for i in range(len(hidden)-1)]
+        self.conv_combined = nn.Sequential(*self.conv)
+        
+        self.avgpool = nn.AvgPool2d(window)
+        self.flattened = hidden_linear[0]
+        self.linear= [DeepSearchLinear(hidden_linear[i], hidden_linear[i+1]) for i in range(len(hidden_linear)-1)]
+        self.linear_combined = nn.Sequential(*self.linear)
         
         self.output = nn.Linear(hidden_linear[-1], labels)
     
     def forward(self, x):
         x_1 = self.model_1(x)
-        x_2 = self.model_2(x)
+        
+        x_2= self.model_2(x)
+        
         x_3 = self.model_3(x)
+        
         x_4 = self.model_4(x)
+        
         x = x_1+ x_2 +x_3 +x_4
+        x = self.batch_norm(x)
         x = self.conv_combined(x)
         x = self.avgpool(x)
         x = x.view(-1, self.flattened)
