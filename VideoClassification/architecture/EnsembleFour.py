d59f6b4bd945edbaf01c934b57bf566904486aac
sahana477 <84558364+sahana477@users.noreply.github.com>
2023-04-21 20:09:09 +0800

    updated with normalization

--- VideoClassification/architecture/EnsembleFour.py
+++ VideoClassification/architecture/EnsembleFour.py
@@ -37,15 +37,6 @@ class DeepSearchLinear(torch.nn.Module):
 class DeepSearch(torch.nn.Module):
     def __init__(self, hidden, kernel_size, window, hidden_linear, labels=2):
         super(DeepSearch, self).__init__()
-        
-        self.conv = [DeepSearchConv(hidden[i], hidden[i+1],  
-                                    kernel_size) for i in range(len(hidden)-1)]
-        self.conv_combined = nn.Sequential(*self.conv)
-        
-        self.avgpool = nn.AvgPool2d(window)
-        self.flattened = hidden_linear[0]
-        self.linear= [DeepSearchLinear(hidden_linear[i], hidden_linear[i+1]) for i in range(len(hidden_linear)-1)]
-        self.linear_combined = nn.Sequential(*self.linear)
         resnet = torchvision.models.resnet50(weights = torchvision.models.ResNet50_Weights.DEFAULT)
         alexnet = torchvision.models.alexnet(weights = torchvision.models.AlexNet_Weights.DEFAULT)
         convexnet = torchvision.models.convnext_base(weights = torchvision.models.ConvNeXt_Base_Weights.DEFAULT)
@@ -62,16 +53,31 @@ class DeepSearch(torch.nn.Module):
         self.model_2 = alexnet
         self.model_3 = convexnet
         self.model_4 = googlenet
+
+        self.batch_norm = nn.BatchNorm2d(hidden[0])
         
+        self.conv = [DeepSearchConv(hidden[i], hidden[i+1],  
+                                    kernel_size) for i in range(len(hidden)-1)]
+        self.conv_combined = nn.Sequential(*self.conv)
+        
+        self.avgpool = nn.AvgPool2d(window)
+        self.flattened = hidden_linear[0]
+        self.linear= [DeepSearchLinear(hidden_linear[i], hidden_linear[i+1]) for i in range(len(hidden_linear)-1)]
+        self.linear_combined = nn.Sequential(*self.linear)
         
         self.output = nn.Linear(hidden_linear[-1], labels)
     
     def forward(self, x):
         x_1 = self.model_1(x)
+        
         x_2= self.model_2(x)
+        
         x_3 = self.model_3(x)
+        
         x_4 = self.model_4(x)
+        
         x = x_1+ x_2 +x_3 +x_4
+        x = self.batch_norm(x)
         x = self.conv_combined(x)
         x = self.avgpool(x)
         x = x.view(-1, self.flattened)
